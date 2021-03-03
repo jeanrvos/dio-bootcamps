07c9f5923efd9cd205e94cc56aba5ac0737f2f24
jeanrvos <velasco.jean@gmail.com>
2021-03-03 08:25:57 -0300

    Inclui alterações referentes a aula de Introdução a JavaScript

---
+++ HTML Web Developer/06 - Programacao para internet com JavaScript/js/main.js
@@ -0,0 +1,109 @@
+
+
+
+/* function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+
+    // console.log(document.getElementById("agradecimento"));
+    // alert ("Obrigado por clicar");
+}
+
+function redirecionar (){
+    window.open("https://globallabs.academy");
+    // window.location.href ="https://globallabas.academy/";
+}
+
+function trocar (elemento) {
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    // document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    // alert("trocar texto");
+}
+
+function voltar (elemento) {
+    elemento.innerHTML = "Passe o mouse aqui";
+    // document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+function load() {
+    alert("Página carregada")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+} */
+
+/* // function soma (n1, n2) {
+//     return n1 + n2;
+// }
+
+function validaIdade (idade) {
+    var validar;
+
+    if (idade >= 18) {
+        validar = true;
+    } else {
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt ("Qual a sua idade?");
+console.log (validaIdade(idade));
+
+// alert(soma(5,10)); */
+
+
+/* var d = new Date();
+// alert(d);
+// alert(d.getMonth()+1); // sempre colocar +1, pois ele começa a contar do zero
+alert(d.getDay()); */
+/* for (count = 0; count <= 5; count++) {
+    alert(count);
+}; */
+
+
+
+/* var count = 0;
+
+while(count <= 5){
+    console.log(count);
+    count++;
+} */
+
+
+
+/* var idade = prompt("Qual sua idade?");
+if (idade >= 18) {
+    alert("Maior de idade");
+} else {
+    alert("Menor de idade");
+} */
+
+
+
+/* var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "verde"}];
+console.log(frutas[1].nome); */
+
+/* var fruta = {nome: "maçã", cor: "vermelha"};
+console.log(fruta.nome); */
+
+/* var lista = ["maçã", "pêra", "laranja"];
+lista.push("uva");
+lista.pop();
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));
+console.log(lista); */
+
+
+
+/* var nome = "Jean Velasco";
+var idade = 31;
+var frase = "Barcelona é o melhor time do mundo!"
+var novaFrase = frase.replace("Barcelona", "Flamengo")
+// alert(nome + " tem " + idade + " anos de idade.");
+console.log(nome + " tem " + idade + " anos de idade.");
+console.log(frase);
+console.log(frase.replace("Barcelona", "Flamengo"));
+console.log(novaFrase.toUpperCase()); // ou string.toLowerCase */
